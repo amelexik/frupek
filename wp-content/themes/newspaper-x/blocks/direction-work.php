bdb68df2a32a88575b136e7fa4af2de53ff4ed99
amelexik <amelexik@gmail.com>
2020-06-19 18:10:32 +0300

    add blocks

---
+++ wp-content/themes/newspaper-x/blocks/direction-work.php
@@ -0,0 +1,98 @@
+<div id="newspaper_x_widget_direction_work" class="widget newspaper_x_widgets">
+    <h3 class="widget-title"><span>Напрямки роботи</span></h3>
+    <div class="row newspaper-x-layout-b-row">
+        <div class="col-md-4 col-xs-6">
+            <div class="newspaper-x-blog-post-layout-b border item-icon">
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12">
+                        <div class="newspaper-x-image">
+                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <div class="newspaper-x-title">
+                            <h3>
+                                <a href="#">Участь у законодавчих ініціативах</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xs-6">
+            <div class="newspaper-x-blog-post-layout-b border item-icon">
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12">
+                        <div class="newspaper-x-image">
+                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <div class="newspaper-x-title">
+                            <h3>
+                                <a href="#">Лобіювання інтересів роботодавців ПЕК України</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xs-6">
+            <div class="newspaper-x-blog-post-layout-b border item-icon">
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12">
+                        <div class="newspaper-x-image">
+                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <div class="newspaper-x-title">
+                            <h3>
+                                <a href="#">Контроль виконання Генеральної та Галузевих угод між
+                                    органами державної влади, роботодавцями та профспілками</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xs-6">
+            <div class="newspaper-x-blog-post-layout-b border item-icon">
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12">
+                        <div class="newspaper-x-image">
+                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <div class="newspaper-x-title">
+                            <h3>
+                                <a href="#">Участь, представництво та захист інтересів членів в
+                                    національній тристоронній соціально – економічній Раді при
+                                    Президенті України та інших державних структурах</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-xs-6">
+            <div class="newspaper-x-blog-post-layout-b border item-icon">
+                <div class="row">
+                    <div class="col-sm-5 col-xs-12">
+                        <div class="newspaper-x-image">
+                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
+                        </div>
+                    </div>
+                    <div class="col-sm-7 col-xs-12">
+                        <div class="newspaper-x-title">
+                            <h3>
+                                <a href="#">Проведення галузевих заходів</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
