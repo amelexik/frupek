82152c4e678711096c13ab86c3c22082b90d8c7e
amelexik <amelexik@gmail.com>
2020-06-21 13:23:40 +0300

    direction-work

--- wp-content/themes/newspaper-x/blocks/direction-work.php
+++ wp-content/themes/newspaper-x/blocks/direction-work.php
@@ -1,98 +1,50 @@
+<?php
+$data = [
+	[
+		'image' => '001-agreement.svg',
+		'text'  => 'Співпраця з державними колами'
+	],
+	[
+		'image' => '002-shield.svg',
+		'text'  => 'Участь у законодавчих ініціативах'
+	],
+	[
+		'image' => '003-presentation.svg',
+		'text'  => 'Лобіювання інтересів роботодавців ПЕК України'
+	],
+	[
+		'image' => '004-share.svg',
+		'text'  => 'Контроль виконання Генеральної та Галузевих угод між органами державної влади, роботодавцями та профспілками'
+	],
+	[
+		'image' => '005-target.svg',
+		'text'  => 'Участь, представництво та захист інтересів членів в національній тристоронній соціально – економічній Раді при Президенті України та інших державних структурах'
+	],
+	[
+		'image' => '006-shout.svg',
+		'text'  => 'Проведення галузевих заходів'
+	],
+]
+?>
+
 <div id="newspaper_x_widget_direction_work" class="widget newspaper_x_widgets">
     <h3 class="widget-title"><span>Напрямки роботи</span></h3>
     <div class="row newspaper-x-layout-b-row">
-        <div class="col-md-4 col-xs-6">
-            <div class="newspaper-x-blog-post-layout-b border item-icon">
-                <div class="row">
-                    <div class="col-sm-5 col-xs-12">
-                        <div class="newspaper-x-image">
-                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-7 col-xs-12">
-                        <div class="newspaper-x-title">
-                            <h3>
-                                <a href="#">Участь у законодавчих ініціативах</a>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-xs-6">
-            <div class="newspaper-x-blog-post-layout-b border item-icon">
-                <div class="row">
-                    <div class="col-sm-5 col-xs-12">
-                        <div class="newspaper-x-image">
-                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-7 col-xs-12">
-                        <div class="newspaper-x-title">
-                            <h3>
-                                <a href="#">Лобіювання інтересів роботодавців ПЕК України</a>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-xs-6">
-            <div class="newspaper-x-blog-post-layout-b border item-icon">
-                <div class="row">
-                    <div class="col-sm-5 col-xs-12">
-                        <div class="newspaper-x-image">
-                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-7 col-xs-12">
-                        <div class="newspaper-x-title">
-                            <h3>
-                                <a href="#">Контроль виконання Генеральної та Галузевих угод між
-                                    органами державної влади, роботодавцями та профспілками</a>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-xs-6">
-            <div class="newspaper-x-blog-post-layout-b border item-icon">
-                <div class="row">
-                    <div class="col-sm-5 col-xs-12">
-                        <div class="newspaper-x-image">
-                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-7 col-xs-12">
-                        <div class="newspaper-x-title">
-                            <h3>
-                                <a href="#">Участь, представництво та захист інтересів членів в
-                                    національній тристоронній соціально – економічній Раді при
-                                    Президенті України та інших державних структурах</a>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 col-xs-6">
-            <div class="newspaper-x-blog-post-layout-b border item-icon">
-                <div class="row">
-                    <div class="col-sm-5 col-xs-12">
-                        <div class="newspaper-x-image">
-                            <span class="glyphicon glyphicon-list-alt" aria-hidden="true"></span>
-                        </div>
-                    </div>
-                    <div class="col-sm-7 col-xs-12">
-                        <div class="newspaper-x-title">
-                            <h3>
-                                <a href="#">Проведення галузевих заходів</a>
-                            </h3>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+	    <?php foreach ($data as $datum) { ?>
+		    <div class="col-md-4 col-xs-6">
+			    <div class="newspaper-x-blog-post-layout-b border item-icon">
+				    <div class="row">
+					    <div class="col-sm-5 col-xs-12">
+						    <div class="newspaper-x-image" style="max-height: 100% !important;">
+							    <img  src="<?= get_template_directory_uri() . '/assets/images/icons/' . $datum['image']; ?>">
+						    </div>
+					    </div>
+					    <div class="col-sm-7 col-xs-12">
+						    <div class="newspaper-x-content"><?= $datum['text']; ?></div>
+					    </div>
+				    </div>
+			    </div>
+		    </div>
+	    <?php } ?>
     </div>
 </div>
